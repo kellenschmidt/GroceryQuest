a705fd2dca0e2f29ff960973a697ce621f2f3264
Kellen Schmidt <kellenschmidt31@gmail.com>
2016-11-09 15:55:38 -0600

    Implemented user-defined and alphabetical sorting of list items

--- app/item-editor/item-editor.component.ts
+++ app/item-editor/item-editor.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'item-editor',
-  templateUrl: './app/item-editor/item-editor.html',
-  styleUrls: [ './app/item-editor/item-editor.css' ]
+	selector: 'item-editor',
+	templateUrl: './app/item-editor/item-editor.html',
+	styleUrls: ['./app/item-editor/item-editor.css']
 })
 
 export class ItemEditorComponent {
@@ -13,15 +13,40 @@ export class ItemEditorComponent {
 
 	_itemEntry: any;
 
-	constructor(){
+	constructor() {
 		this.placeholder = "Item name...";
 		this._itemEntry = {};
 	}
 
-	addItem(){
+	addItem() {
 		this._itemEntry.list_id = this.model.length + 1;
 		this.model.push(this._itemEntry);
 		this._itemEntry = {};
+	}
+
+	swapItems(index1: number, index2: number) {
+		// Skip swaps for out of range indices
+		if(index1 < 0 || index2 >= this.model.length) {
+			return;
+		}
+		// Get element at index2
+		let tempItem: any[] = this.model.slice(index2, index2 + 1);
+		// Copy element at index1 to element at index2
+		this.model.copyWithin(index2, index1, index1 + 1);
+		// Set element at index1 equal to element at index2
+		this.model[index1] = tempItem[0];
+	}
+
+	// Sort json item objects by name using custom compare function
+	sortItems() {
+		this.model.sort(function(a: any, b: any):number {
+			if(a.name < b.name)
+				return -1;
+			else if (a.name > b.name)
+				return 1;
+			else
+				return 0;
+		});
 	}
 
 }
